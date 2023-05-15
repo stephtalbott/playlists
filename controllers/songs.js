4e3f8c415c9e3d8282fb32259be663f154c7105a
stephtalbott <talbottstephanie@ymail.com>
2023-05-15 13:27:03 -0500

    can delete songs from playlists now

--- controllers/songs.js
+++ controllers/songs.js
@@ -14,7 +14,19 @@ function addSongToPlaylist(req, res, next) {
         .catch(next)    
 }
 
+function deleteSongFromPlaylist(req, res, next) {
+  Playlist.findById(req.params.playlistId)
+    .then((playlist) => {
+      if (!playlist.user.equals(req.user._id))
+        throw new Error("Unauthorized");
+      playlist.song.id(req.params.songId).deleteOne();
+      return playlist.save();
+    })
+    .then(() => res.redirect(`/playlists/${req.params.playlistId}`))
+    .catch(next);
+}
 
 module.exports = {
     addSongToPlaylist,
+    deleteSongFromPlaylist,
 }
--- routes/songs.js
+++ routes/songs.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const songCtrl = require('../controllers/songs')
+const song = require('../models/song')
 
 
 router.post('/playlists/:playlistId', songCtrl.addSongToPlaylist)
+
+
+router.delete('/playlists/:playlistId/:songId', songCtrl.deleteSongFromPlaylist) 
+
 
 module.exports = router
